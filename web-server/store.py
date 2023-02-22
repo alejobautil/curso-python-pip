e20d4051feb79458158cff065b0f406a8f088fb8
alejobautil <diego.bautista2808@gmail.com>
2023-02-22 16:34:59 -0500

    Creación d euna carpeta llamada web service, que se necarga de hacer request a un api

---
+++ web-server/store.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json() #Me lo transforma desde un string a una lista
+    for category in categories:
+        print(category['name'])
